67aa311df06b858ce6235281217ebea486bd2361
SpyrosSiannas <ssiannas@gmail.com>
2022-05-27 21:03:24 +0300

    Fix tensor not converting entire list

--- GAN/trainloop.py
+++ GAN/trainloop.py
@@ -54,8 +54,9 @@ class TrainLoop():
         self.writer_real = SummaryWriter(f"logs/real")
         self.writer_fake = SummaryWriter(f"logs/fake")
         self.fixed_noise = torch.randn(32, self.params.Z_DIM, 1, 1).to(self.device)
-        self.fixed_attrs = torch.tensor([self.dataset[i][1] for i in range(32)])
-        self.fixed_attrs = expand_labels_for_input(self.fixed_noise, ).to(self.device)
+        self.fixed_attrs = [self.dataset[i][1] for i in range(32)]
+        self.fixed_attrs = torch.tensor(self.fixed_attrs).to(self.device)
+        self.fixed_attrs = expand_labels_for_input(self.fixed_noise, self.fixed_attrs ).to(self.device)
 
     def predict(self, features_vector):
         if (self.__load_model("cpu")):
