418a79ec87ce6deea10ae7ef7c2d4a2cad463d31
SpyrosSiannas <ssiannas@gmail.com>
2022-05-27 20:22:52 +0300

    Correct fixed label parsing

--- GAN/trainloop.py
+++ GAN/trainloop.py
@@ -54,8 +54,7 @@ class TrainLoop():
         self.writer_real = SummaryWriter(f"logs/real")
         self.writer_fake = SummaryWriter(f"logs/fake")
         self.fixed_noise = torch.randn(32, self.params.Z_DIM, 1, 1).to(self.device)
-        _, self.fixed_attrs = self.dataset[:32]
-
+        self.fixed_attrs = [self.dataset[i][1] for i in range(32)]
 
     def predict(self, features_vector):
         if (self.__load_model("cpu")):
